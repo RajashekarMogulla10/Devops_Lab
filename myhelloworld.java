e952e5ec0e731ff7196661917ebd4dcdd18a322f
Rajashekar <b200371@rgukt.ac.in>
2025-03-06 15:16:44 +0530

    hello.java

---
+++ myhelloworld.java
@@ -0,0 +1,15 @@
+class myhelloworld
+{
+	void print()
+	{
+	System.out.println("HELLOWORLD");
+	}
+}
+class hello
+{
+	public static void main(String[] args)
+	{
+	myhelloworld hw=new myhelloworld();
+	hw.print();
+	}
+}
